e01f2aca6780bff40dc58aee4a386308599573f1
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-07-02 14:03:19 +0530

    Added hero HTML structure

---
+++ assets/js/animations.js
@@ -0,0 +1,21 @@
+AOS.init({
+
+    duration:1000,
+
+    once:true,
+
+    offset:100
+
+});
+
+VanillaTilt.init(document.querySelector(".profile-card"),{
+
+    max:12,
+
+    speed:400,
+
+    glare:true,
+
+    "max-glare":0.25
+
+});
---
+++ assets/js/typing.js
@@ -0,0 +1,23 @@
+new Typed("#typing", {
+
+    strings: [
+
+        "Aspiring Software Engineer",
+        "BCA Student",
+        "Python Developer",
+        "AI Developer",
+        "I know C programming",
+        "Microsoft learned"
+        
+
+    ],
+
+    typeSpeed: 70,
+
+    backSpeed: 40,
+
+    backDelay: 1500,
+
+    loop: true
+
+});
