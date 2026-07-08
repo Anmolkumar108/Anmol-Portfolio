1eaa0cc495b5dbbe76919af9a07235bf8db97ad5
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-07-08 16:47:07 +0530

    कोड अपडेट किया

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -5,4 +5,10 @@ heroBackgrounds.forEach(heroBackground => {
     canvas.className = 'hero-particles';
     heroBackground.appendChild(canvas);
 
+    const ctx = canvas.getContext('2d');
+    let width = 0;
+    let height = 0;
+    let particles = [];
+    let animationFrameId = null;
+
     
